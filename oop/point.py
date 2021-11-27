3740b2c91d610a29d32a5620e31a3289e02193b2
二之前一 <github@caofeng.me>
2021-11-27 10:44:31 +0800

    basic notebook

--- oop/point.py
+++ oop/point.py
@@ -1,46 +1,28 @@
 class Point:
-    def __init__(self, x, y):
+    """ represents a point in 2-D geometric coordinates """
+
+    def __init__(self, x=0, y=0):
+        """
+        initializes the position of a new point. the x  and y  coordinates can be specified.
+        if they are not, the point defaults to the origin
+        """
         self.move(x, y)
 
     def reset(self):
+        """
+        Reset the point back to the geometric origin: 0, 0
+        """
         self.move(0, 0)
 
     def move(self, x, y):
+        """move the point back to a new location in 2D space """
         self.x = x
         self.y = y
 
     def calculate_distance(self, other_point):
+        """
+        Calculate the distance from this point to a second point passed as a parameter.
+        This function uses the Pythagorean Theorem to calculate the distance between the two points.
+        the distance is returned as a float.
+        """
         return ((self.x - other_point.x) ** 2 + (self.y - other_point.y) ** 2) ** 0.5
-
-
-p1 = Point()
-p2 = Point()
-
-p1.x = 5
-p1.y = 4
-
-p2.x = 3
-p2.y = 6
-
-print(p1.x, p1.y)
-print(p2.x, p2.y)
-
-p = Point()
-p.reset()
-print(p.x, p.y)
-
-p = Point()
-Point.reset(p)
-print(p.x, p.y)
-
-point1 = Point()
-point2 = Point()
-
-point1.reset()
-point2.move(5, 0)
-print(point1.calculate_distance(point2))
-assert point1.calculate_distance(point2) == point1.calculate_distance(point2)
-
-point1.move(3, 4)
-print(point1.calculate_distance(point2))
-print(point1.calculate_distance(point1))
---
+++ tests/notebook/test_notebook.py
@@ -0,0 +1,93 @@
+import unittest
+
+from notebook.notebook import Note, NoteBook
+
+
+class NoteMatchTestCase(unittest.TestCase):
+
+    def test_match(self):
+        n1 = Note('hello first')
+        match = n1.match('hello')
+        self.assertTrue(match)
+
+    def test_not_match(self):
+        n2 = Note('hello again')
+        match = n2.match('second')
+        self.assertFalse(match)
+
+
+class NewNotebookTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.notebook = NoteBook()
+
+    def test_new_note(self):
+        memo = 'hello world'
+        self.notebook.new_note(memo)
+        self.assertEqual(len(self.notebook.notes), 1)
+        note = self.notebook.notes[0]
+        self.assertEqual(memo, note.memo)
+
+    def test_new_note_tags(self):
+        memo = 'hello world'
+        tags = ['first']
+        self.notebook.new_note(memo, tags=tags)
+        self.assertEqual(len(self.notebook.notes), 1)
+        note = self.notebook.notes[0]
+        self.assertEqual(memo, note.memo)
+        self.assertEqual(tags, note.tags)
+
+
+class SearchNotebookTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.notebook = NoteBook()
+        self.notebook.new_note('hello world')
+        self.notebook.new_note('hello world again')
+        self.notebook.new_note('hello world again and again')
+        self.notebook.new_note('hello again')
+
+    def test_search_notes(self):
+        notes = self.notebook.search('hello')
+        self.assertEqual(len(notes), 4)
+
+    def test_search_notes_no_matches(self):
+        notes = self.notebook.search('goodbye')
+        self.assertEqual(len(notes), 0)
+
+
+class ModifyMemoTestCase(unittest.TestCase):
+    def setUp(self):
+        self.momo = 'hello world'
+        self.notebook = NoteBook()
+        self.notebook.new_note(self.momo)
+
+    def test_modify_memo(self):
+        notes = self.notebook.search(self.momo)
+        self.assertEqual(len(self.notebook.notes), 1)
+        note = notes[0]
+        new_memo = 'new memo'
+        modify = self.notebook.modify_memo(note.id, new_memo)
+        self.assertTrue(modify)
+        note = self.notebook._find_note(note.id)
+        self.assertEqual(new_memo, note.memo)
+
+
+class ModifyTagsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.momo = 'hello world'
+        self.notebook = NoteBook()
+        self.notebook.new_note(self.momo)
+
+    def test_modify_tags(self):
+        notes = self.notebook.search(self.momo)
+        self.assertEqual(len(self.notebook.notes), 1)
+        note = notes[0]
+        new_tags = ['new', 'tags']
+        modify = self.notebook.modify_tags(note.id, new_tags)
+        self.assertTrue(modify)
+        note = self.notebook._find_note(note.id)
+        self.assertEqual(new_tags, note.tags)
+
+if __name__ == '__main__':
+    unittest.main()
